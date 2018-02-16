1c26e6837dd842213d024e02e5b20a30ede4a51d
Ian Monroe <ian@ianmonroe.com>
2018-02-15 23:39:18 -0600

    a good starting place.

---
+++ resources/assets/js/components/googleTaskList.vue
@@ -0,0 +1,57 @@
+<style scoped>
+
+</style>
+
+
+
+<template>
+
+    <div>
+          <p>Holding pattern.<p/>  
+    </div>
+
+</template>
+
+
+
+<script>
+export default {
+    components: {},
+    mixins: [],
+    data () {
+      return {
+        csrf: '',
+        taskList: ''
+      }
+    },
+    mounted() {
+        this.csrf = window.axios.defaults.headers.common['X-CSRF-TOKEN'];
+        this.fetchList();
+    },
+    props: [
+        'aspectId',
+        'aspectType',
+        'aspectNotes',
+        'aspectData',
+        'aspectSource',
+        'hidden',
+        'subjectId',
+        'settingsListId',
+        'title'
+    ],
+    computed: {},
+    methods: {
+        fetchList(){
+            var self = this;
+            axios.get('/gtasks/list/'+self.settingsListId)
+            .then(function(response){
+                self.taskList = response.data;
+                console.log(response.data);
+            })
+            .catch(function(error){
+                console.log(error);
+            });
+        }
+    }
+};
+</script>
