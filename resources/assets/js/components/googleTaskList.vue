56e28befa3af212b2036c688ab701832520dbbf4
Ian Monroe <ian@ianmonroe.com>
2018-02-16 00:49:09 -0600

    we'd prefer a string to an object.

--- resources/assets/js/components/googleTaskList.vue
+++ resources/assets/js/components/googleTaskList.vue
@@ -15,7 +15,7 @@
 
         <form id="new_task" class="form-inline my-2 my-lg-0" v-on:submit.prevent="addNewTask">
             <input type="hidden" name="_token" :value="csrf">
-            <input type="hidden" name="task_list" :value="taskList">
+            <input type="hidden" name="task_list" :value="settingsListId">
 
             <div class="form-group">
                 <input type="text" class="form-control" id="new_task_title" name="new_task_title" placeholder="Add a new task" v-model="new_task_title" >
