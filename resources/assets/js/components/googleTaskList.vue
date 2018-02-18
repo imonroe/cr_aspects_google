e5d1b03feccb672ad968f9a927261108d948cad3
Ian Monroe <ian@ianmonroe.com>
2018-02-18 16:32:29 -0600

    tinker

--- resources/assets/js/components/googleTaskList.vue
+++ resources/assets/js/components/googleTaskList.vue
@@ -63,7 +63,7 @@ export default {
             .catch(function(error){
                 console.log(error);
             });
-            jQuery.rejigger();
+            window.$.rejigger();
         }, 
         addNewTask(){
             var self = this;
