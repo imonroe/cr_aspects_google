818b99af7df89e84b127a618f66439f55f604d67
Ian Monroe <ian@ianmonroe.com>
2018-02-18 14:07:30 -0600

    getting started on Calendar

---
+++ resources/assets/js/components/googleCalendar.vue
@@ -0,0 +1,79 @@
+<style scoped>
+
+</style>
+
+<template>
+
+    <div>
+        <div id="calendar_widget"></div>
+
+
+    </div>
+
+</template>
+
+
+<script>
+    export default {
+        components: {},
+        mixins: [],
+        data () {
+            return {
+                csrf: '',
+                calendarId: '',
+                calendar: ''
+            }
+        },
+        mounted() {
+            this.csrf = window.axios.defaults.headers.common['X-CSRF-TOKEN'];
+            this.fetchCalendar();
+            $("#calendar_widget").datepicker();
+        },
+        props: [
+            'aspectId',
+            'aspectType',
+            'aspectNotes',
+            'aspectData',
+            'aspectSource',
+            'hidden',
+            'subjectId',
+            'settingsCalendarId',
+            'title'
+        ],
+        computed: {
+            doUpdate: function(){
+                if (typeof this.aspectId === 'undefined' || this.aspectId === null) {
+                    return false;
+                } else {
+                    return true;
+                }
+            },
+            actionPath: function(){
+                if ( this.doUpdate ){
+                    var action_path = '/aspect/' + this.aspectId + '/edit';
+                    return action_path;
+                } else {
+                    return '/aspect/create';
+                }
+            }
+        },
+        methods: {
+            fetchCalendar(){
+                var self=this;
+                axios.get('/gcal/available_calendars')
+                    .done(function(response){
+                        self.calendarList = response;
+                        console.log(response);
+                    })
+                    .error(function(error){
+                        console.log(error);
+                    });
+
+            },
+            createCalendar(){
+
+            }
+        }
+    };
+
+</script>
