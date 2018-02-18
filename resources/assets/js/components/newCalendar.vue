e53aff4772337ab2f48e35ccfa9ad12d51860e7a
Ian Monroe <ian@ianmonroe.com>
2018-02-17 18:57:47 -0600

    try out a selection form.

--- resources/assets/js/components/newCalendar.vue
+++ resources/assets/js/components/newCalendar.vue
@@ -16,7 +16,32 @@
         </div>
 
         <div>
-            select a calendar
+            
+            <form v-if="this.doUpdate == false" id="select_google_calendar" class="form-inline my-2 my-lg-0" method="POST" :action="actionPath" >
+            <!-- the create version. -->
+                <input type="hidden" name="_token" :value="csrf"></input>
+                <input type="hidden" name="aspect_type" :value="aspectType"></input>
+                <input type="hidden" name="subject_id" :value="subjectId"></input>
+                <input type="hidden" name="aspect_data" value=""></input>
+                <input type="hidden" name="hidden" value="0"></input>
+                <input type="hidden" name="aspect_source" value="Google"></input>
+
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" id="title" name="title"></input>        
+                </div>
+
+                <div class="form-group">
+                    <label for="settings_calendar_id">Use this calendar</label>
+                    <select class="form-control" id="settings_calendar_id" name="settings_calendar_id">
+                        <option disabled value=""> -- Select a calendar -- </option>
+                        <option v-for="calendar in calendarList.data.items" :key="calendar.id" v-bind:value="calendar.id"> {{ calendar.summary }} </option>
+                    </select>
+                </div>
+                
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+            
         </div>
 
     </div>
@@ -32,7 +57,8 @@
         data () {
             return {
                 csrf: '',
-                calendarList: ''
+                calendarList: '',
+                calendarId: ''
             }
         },
         mounted() {
@@ -47,10 +73,26 @@
             'aspectSource',
             'hidden',
             'subjectId',
-            'settingsListId',
+            'settingsCalendarId',
             'title'
         ],
-        computed: {},
+        computed: {
+            doUpdate: function(){
+                if (typeof this.aspectId === 'undefined' || this.aspectId === null) {
+                    return false;
+                } else {
+                    return true;
+                }
+            },
+            actionPath: function(){
+                if ( this.doUpdate ){
+                    var action_path = '/aspect/' + this.aspectId + '/edit';
+                    return action_path;
+                } else {
+                    return '/aspect/create';
+                }
+            }
+        },
         methods: {
             fetchCalendars(){
                 var self=this;
